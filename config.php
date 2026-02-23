d1c1494dfc43b6111d70c9da4b31ae17f8055899
Sebastiaan Stok <s.stok@rollerscapes.net>
2026-02-23 12:03:37 +0100

    Mark 1.x branch as unmaintained

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'schema_version' => 2,
+    'host' => 'github.com',
+    'repository' => 'rollerworks/PasswordStrengthBundle',
+    'main_branch' => 'main',
+    'branches' => [
+        '1.x' => false,
+    ],
+];
